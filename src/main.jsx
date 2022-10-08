4f3e5e756fd9e3a80c91bc1e43203fc5d22a6c07
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-10-08 16:08:35 +0530

    added css style

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import "./css/style.css";
 const domain = import.meta.env.VITE_AUTH0;
 const clientId = import.meta.env.VITE_CLIENT_ID;
 
